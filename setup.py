72cfc6f1625b52acdb5760f656c84089890145a3
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-05-04 13:25:01 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='almavik',
-    version='0.1.1',
+    version='0.1.2',
     description='Determination of the drop trajectory',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
