22dedaec45932659722eb3517ab01f08766c0f6f
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-06-17 20:56:06 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from io import open
+
+setup(
+    name='almavik',
+    version='0.2.10',
+    description='Determination of the drop trajectory',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/markusLons/almavik',
+    author='Markus.Alinas.Viktorias',
+    author_email='v.tikhonova@g.nsu.ru',
+    packages= ['almavik', 'almavik.exp1'],
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'PyQt5',
+        'matplotlib',
+    ]
+)
