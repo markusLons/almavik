b2bccf306c1ae9f6293c77716fd4b9d7467864b7
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-06-20 01:55:12 +0700

    Update test_detect.py

--- src/tests/test_detect.py
+++ src/tests/test_detect.py
@@ -3,122 +3,57 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
+import unittest
 sys.path.insert(1, 'src/')
 from detectorDrop import detectorDrop
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 exp1_dir = os.path.join(current_dir, '../exp1')
 
-# Теперь вы можете использовать переменную exp1_dir для обращения к папке exp1
+class TestDetectorDrop(unittest.TestCase):
 
-# Путь к тестовым изображениям
-TEST_IMAGE_DIR = exp1_dir
+    def setUp(self):
+        # Создаем экземпляр класса detectorDrop
+        self.detector = detectorDrop(exp1_dir)
 
-def test_shaded_contours():
-    # Создаем объект класса detectorDrop
-    detector = detectorDrop()
+    def test_center_mass(self):
+        # Проверяем, что center_mass является списком
+        self.assertIsInstance(self.detector.center_mass, list)
 
-    # Создаем тестовое изображение
-    image = np.zeros((100, 100, 3), dtype=np.uint8)
+    def test_img(self):
+        # Проверяем, что img является списком
+        self.assertIsInstance(self.detector.img, list)
 
-    # Вызываем метод shaded_contours и проверяем результат
-    result = detector.shaded_contours(image)
-    assert result.shape == image.shape
-    assert result.dtype == np.float64
+    def test_img_contour(self):
+        # Проверяем, что img_contour является списком
+        self.assertIsInstance(self.detector.img_contour, list)
 
-def test_detectorDrop_initialization():
-    # Создаем объект класса detectorDrop с тестовым путем к изображениям
-    detector = detectorDrop(TEST_IMAGE_DIR)
+    def test_shaded_contours(self):
+        # Создаем фиктивное изображение для тестирования
+        image = np.zeros((100, 100, 3), dtype=np.uint8)
+        result = self.detector.shaded_contours(image)
+        # Проверяем, что результат имеет тот же размер, что и исходное изображение
+        self.assertEqual(result.shape, image.shape)
 
-    # Проверяем, что список изображений не пустой
-    assert len(detector.img) > 0
+    def test_init(self):
+        # Проверяем, что после инициализации экземпляра класса, все списки имеют одинаковую длину
+        self.assertEqual(len(self.detector.center_mass), len(self.detector.img))
+        self.assertEqual(len(self.detector.center_mass), len(self.detector.img_contour))
 
-    # Проверяем, что списки center_mass и img_contour также не пустые
-    assert len(detector.center_mass) > 0
-    assert len(detector.img_contour) > 0
+        # Проверяем, что каждый элемент списка center_mass является кортежем с тремя значениями
+        for item in self.detector.center_mass:
+            self.assertIsInstance(item, tuple)
+            self.assertEqual(len(item), 3)
 
-def test_detectorDrop_methods():
-    # Создаем объект класса detectorDrop с тестовым путем к изображениям
-    detector = detectorDrop(TEST_IMAGE_DIR)
+        # Проверяем, что каждый элемент списка img является изображением numpy array
+        for img in self.detector.img:
+            self.assertIsInstance(img, np.ndarray)
+            self.assertEqual(img.ndim, 3)  # Проверяем, что изображение имеет 3 канала (BGR)
 
-    # Проверяем, что методы работают без ошибок
-    assert len(detector.img) == len(detector.img_contour) == len(detector.center_mass)
+        # Проверяем, что каждый элемент списка img_contour является изображением numpy array
+        for img_contour in self.detector.img_contour:
+            self.assertIsInstance(img_contour, np.ndarray)
+            self.assertEqual(img_contour.ndim, 3)  # Проверяем, что изображение имеет 3 канала (BGR)
 
-    # Проверяем, что все элементы в списке center_mass имеют правильный формат
-    for item in detector.center_mass:
-        assert isinstance(item, tuple)
-        assert len(item) == 3
-        assert isinstance(item[0], int)
-        assert isinstance(item[1], int)
-        assert isinstance(item[2], int)
-
-    # Проверяем, что все элементы в списке img и img_contour являются изображениями
-    for img, img_contour in zip(detector.img, detector.img_contour):
-        assert isinstance(img, np.ndarray)
-        assert isinstance(img_contour, np.ndarray)
-        assert img.shape[2] == 3
-        assert img_contour.shape[2] == 3
-
-        # Проверяем, что изображения имеют правильный тип данных
-        assert img.dtype == np.uint8
-        assert img_contour.dtype == np.uint8
-
-        # Проверяем, что изображения не являются пустыми
-        assert img.size > 0
-        assert img_contour.size > 0
-
-def test_detectorDrop_process_images():
-    # Создаем объект класса detectorDrop с тестовым путем к изображениям
-    detector = detectorDrop(TEST_IMAGE_DIR)
-
-    # Обрабатываем изображения и проверяем результаты
-    for img, img_contour in zip(detector.img, detector.img_contour):
-        assert img.shape == img_contour.shape
-
-        # Проверяем, что изображения имеют правильный тип данных после обработки
-        assert img.dtype == np.uint8
-        assert img_contour.dtype == np.uint8
-
-        # Проверяем, что изображения не являются пустыми
-        assert img.size > 0
-        assert img_contour.size > 0
-
-        # Проверяем, что изображения имеют правильное количество каналов
-        assert img.shape[2] == 3
-        assert img_contour.shape[2] == 3
-
-        # Проверяем, что обработанные изображения имеют правильный тип данных
-        assert img_contour.dtype == np.uint8
-
-        # Проверяем, что обработанные изображения не являются пустыми
-        assert img_contour.size > 0
-
-def test_detectorDrop_center_of_mass():
-    # Создаем объект класса detectorDrop с тестовым путем к изображениям
-    detector = detectorDrop(TEST_IMAGE_DIR)
-
-    # Проверяем, что список center_mass не пустой
-    assert len(detector.center_mass) > 0
-
-    # Проверяем, что все элементы в списке center_mass имеют правильный формат
-    for item in detector.center_mass:
-        assert isinstance(item, tuple)
-        assert len(item) == 3
-        assert isinstance(item[0], int)
-        assert isinstance(item[1], int)
-        assert isinstance(item[2], int)
-
-def test_plot_center_of_mass():
-    # Создаем объект класса detectorDrop с тестовым путем к изображениям
-    detector = detectorDrop(TEST_IMAGE_DIR)
-
-    # Проверяем, что список center_mass не пустой
-    assert len(detector.center_mass) > 0
-
-    # Проверяем, что график успешно строится без ошибок
-    detector.plot_center_of_mass()
-
-    # Проверяем, что график отображается
-    assert plt.fignum_exists(1)
-
+if __name__ == '__main__':
+    unittest.main()
