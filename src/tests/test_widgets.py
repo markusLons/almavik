8ab914dc98280a12fed57374bea245d37ef00e30
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-06-20 00:54:28 +0700

    Update test_widgets.py

--- src/tests/test_widgets.py
+++ src/tests/test_widgets.py
@@ -5,45 +5,76 @@ import sys
 sys.path.insert(1, 'src/')
 from widgets import MplCanvas, LineCanvas, ImageCanvas, Table
 
-@pytest.fixture(scope='module')
-def qt_app(request):
+@pytest.fixture
+def app():
+    # Инициализация QApplication
     app = QApplication([])
-    
-    def teardown():
-        app.quit()
-    
-    request.addfinalizer(teardown)
-    return app
+    yield app
+    app.quit()
 
-def test_MplCanvas_update_canvas(qt_app):
+
+def test_mpl_canvas(app):
+    # Создание экземпляра MplCanvas
     canvas = MplCanvas()
+    
+    # Проверка, что canvas является экземпляром QWidget
+    assert isinstance(canvas, QWidget)
+    
+    # Проверка значений по умолчанию
+    assert canvas.fig.get_figwidth() == 7
+    assert canvas.fig.get_figheight() == 7
+    assert canvas.fig.get_dpi() == 100
+    
+    # Проверка метода update_canvas
     canvas.update_canvas()
-    # No exception should be raised
 
-def test_LineCanvas_draw_line(qt_app):
+
+def test_line_canvas(app):
+    # Создание экземпляра LineCanvas
     canvas = LineCanvas()
+    
+    # Проверка, что canvas является экземпляром MplCanvas
+    assert isinstance(canvas, MplCanvas)
+    
+    # Проверка значений по умолчанию
+    assert canvas.line is None
+    assert isinstance(canvas.data, list)
+    
+    # Проверка метода draw_line
     canvas.draw_line()
-    # No exception should be raised
 
-def test_ImageCanvas_load_image(qt_app):
+
+def test_image_canvas(app):
+    # Создание экземпляра ImageCanvas
     canvas = ImageCanvas()
+    
+    # Проверка, что canvas является экземпляром MplCanvas
+    assert isinstance(canvas, MplCanvas)
+    
+    # Проверка значений по умолчанию
+    assert canvas.img is None
+    assert isinstance(canvas.img_without_contour, list)
+    assert isinstance(canvas.img_with_contour, list)
+    
+    # Проверка методов load_image, draw_next_image, draw_previous_image, update_image_from_slider
     canvas.load_image()
-    # No exception should be raised
+    canvas.draw_next_image()
+    canvas.draw_previous_image()
+    canvas.update_image_from_slider(50)
 
-def test_Table_show_row():
+
+def test_table():
+    # Создание экземпляра Table
     table = Table(current_image_idx=0, det=None)
+    
+    # Проверка, что table является экземпляром QWidget
+    assert isinstance(table, QWidget)
+    
+    # Проверка значений по умолчанию
+    assert isinstance(table.data, list)
+    
+    # Проверка методов show_row, numbers_of_rows
     table.show_row(current_image_idx=0)
-    # No exception should be raised
-
-def test_Table_numbers_of_rows():
-    table = Table(current_image_idx=0, det=None)
     row_indices = table.numbers_of_rows()
-    assert row_indices == [0, 1, 2]
+    assert isinstance(row_indices, list)
 
-    table.index = len(table.data) - 1
-    row_indices = table.numbers_of_rows()
-    assert row_indices == [table.index - 2, table.index - 1, table.index]
-
-    table.index = 1
-    row_indices = table.numbers_of_rows()
-    assert row_indices == [table.index - 1, table.index, table.index + 1]
