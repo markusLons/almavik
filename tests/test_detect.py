44cc1a8f8edb97a064b2a630e75b18d14e95f595
Markus Lons <markckv@gmail.com>
2023-06-20 11:36:18 +0700

    Update test_detect.py

--- tests/test_detect.py
+++ tests/test_detect.py
@@ -1,9 +1,9 @@
 import pytest
-from src.detectorDrop import detectorDrop
+from almavik.src.detectorDrop import detectorDrop
 
 def test_detectorDrop_lists_length():
     images_folder = "./exp1"
 
     detector = detectorDrop(images_folder)
 
-    assert len(detector.center_mass) == len(detector.img) == len(detector.img_contour) == 0
+    assert len(detector.center_mass) == len(detector.img) == len(detector.img_contour) == 401
