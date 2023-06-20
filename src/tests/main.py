2720aca4c7b1422d624df478ecdfca90c8e53195
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-06-20 20:46:05 +0700

    Update main.py

--- src/tests/main.py
+++ src/tests/main.py
@@ -1,1 +1,8 @@
+try:
+    from .wow.test_widgets import run_tests as widgets_tests
+    from .wow.test_detect import run_tests as detectr_tests
 
+    widgets_tests()
+    detectr_tests()
+except ImportError as err:
+    print(f"Could't run GUI tests:\n {err}")
