709d387d2b6c5aee3becdafa34f572b50eddca81
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-06-20 22:11:09 +0700

    Update test_main.py

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,7 +1,9 @@
 try:
     from .wow.test_widgets import run_tests as widgets_tests
     from .wow.test_detect import run_tests as detectr_tests
+    from .wow.test_ypprpo import run_tests as windows_test
 
+    windows_test()
     widgets_tests()
     detectr_tests()
 except ImportError as err:
