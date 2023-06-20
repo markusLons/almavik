939cc6f258a9f53cf01d8ceeb8817f9a3d717343
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-06-20 13:20:03 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from io import open
 
 setup(
     name='almavik',
-    version='0.3',
+    version='3.9',
     description='Determination of the drop trajectory',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/markusLons/almavik',
     author='Markus.Alinas.Viktorias',
     author_email='v.tikhonova@g.nsu.ru',
-    packages= ['almavik', 'exp1'],
+    packages= ['almavik', 'almavik.exp1'],
+    package_data={'almavik.exp1': ['*.jpeg']},
+    #packages=find_packages(),
+    #include_package_data=True,
     install_requires=[
         'opencv-python',
         'numpy',
