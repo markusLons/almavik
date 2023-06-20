9eeabeea39bbd2c2077b021c4a5e129d5ca685d0
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-06-20 19:43:25 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 from io import open
 import datetime
 # Генерируем номер версии на основе текущего времени
-version = datetime.datetime.now().strftime('%Y.%m.%d.%H%M')
 
 setup(
-    name='almavik',
+    name="4.2",
     version=version,
     description='Determination of the drop trajectory',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
