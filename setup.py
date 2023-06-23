6e8e47516cf64b36831a38df6372cf3a97ad2e61
Viktoria <124949879+ViktoriaTix@users.noreply.github.com>
2023-06-23 23:56:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url='https://github.com/markusLons/almavik',
     author='Markus.Alinas.Viktorias',
     author_email='v.tikhonova@g.nsu.ru',
-    packages=['src', 'exp1'],
+    packages=['almavik', 'almavik.exp1'],
     package_data={'almavik.exp1': ['*.jpeg']},
     install_requires=[
         'opencv-python',
